6b0910f2da33325a7a470e3ac6225d23d509de5f
dhjoe <ssd351@daum.net>
2022-12-09 15:17:53 +0900

    App06 예쩨 추가

---
+++ src/App06.js
@@ -0,0 +1,35 @@
+// App06.js
+
+import { Component } from "react";
+
+class App extends Component{
+    state = {
+        msg:""
+    }
+
+    render() {
+        return(
+            <div className="container">
+                <h1>form 요소들 사용해보기</h1>
+                {/* form 안에 있는 submit버튼을 누르면 submit이벤트가 발생한다. */}
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="msg">메세지 입력</label>
+                    <input type="text" id="msg" onChange={(e) => {
+                        this.setState({
+                            msg:e.target.value
+                        });
+                    }} value={this.state.msg}/>
+                    <button type="submit">전송</button>
+                </form>
+                <p>현재 입력한 내용 : <strong>{this.state.msg}</strong></p>
+            </div>
+        );
+    }
+    //form에 submit이벤트가 발생하면 호출
+    handleSubmit = (e) => {
+        e.preventDefault();
+        alert(this.state.msg + " 를 전송합니다.");
+    }
+}
+
+export default App;
