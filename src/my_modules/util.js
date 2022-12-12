cde9f8b7aab2c2ccf0fcd39c45d4ddbab3cda8b9
dhjoe <ssd351@daum.net>
2022-12-12 15:42:28 +0900

    App09 예제 추가

---
+++ src/my_modules/util.js
@@ -0,0 +1,14 @@
+
+const drill = () => {
+    console.log("구멍을 뚫어요");
+}
+
+const sender = (msg) => {
+    console.log(msg+" 를 전송합니다.");
+}
+
+//두 개의 함수를 object에 담아서
+const obj = {drill, sender}
+
+//default로 export한다.
+export default obj;
